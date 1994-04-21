3828636d3312994c1651303a052f59eb1773871a
Paul Mackerras <paulus@samba.org>
1994-04-21 03:34:00 +0000

    *** empty log message ***

--- pppd/patchlevel.h
+++ pppd/patchlevel.h
@@ -1,5 +1,5 @@
-/* $Id: patchlevel.h,v 1.5 1994/04/18 04:08:22 paulus Exp $ */
+/* $Id: patchlevel.h,v 1.6 1994/04/21 03:34:00 paulus Exp $ */
 #define	PATCHLEVEL	0
 
-#define VERSION	"2.1"
-#define DATE	"13 Apr 94"
+#define VERSION	"2.1a"
+#define DATE	"21 Apr 94"
