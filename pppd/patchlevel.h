e07cf2fb6746fe0d76ebd19de41833f25c80b0e9
Paul Mackerras <paulus@samba.org>
1996-01-18 03:35:54 +0000

    updated

--- pppd/patchlevel.h
+++ pppd/patchlevel.h
@@ -1,6 +1,6 @@
-/* $Id: patchlevel.h,v 1.19 1996/01/01 23:01:20 paulus Exp $ */
+/* $Id: patchlevel.h,v 1.20 1996/01/18 03:35:54 paulus Exp $ */
 #define	PATCHLEVEL	0
 
 #define VERSION		"2.3"
 #define IMPLEMENTATION	"alpha1"
-#define DATE		"16 Dec 95"
+#define DATE		"18 Jan 96"
