bd88ff6bd52c2ed24d420a3be7bb9c51b9414bcc
Paul Mackerras <paulus@samba.org>
1998-02-04 01:36:40 +0000

    make pppoutput global

--- freebsd-2.0/if_ppp.c
+++ freebsd-2.0/if_ppp.c
@@ -69,7 +69,7 @@
  * Paul Mackerras (paulus@cs.anu.edu.au).
  */
 
-/* $Id: if_ppp.c,v 1.12 1997/04/30 05:42:07 paulus Exp $ */
+/* $Id: if_ppp.c,v 1.13 1998/02/04 01:36:39 paulus Exp $ */
 /* from if_sl.c,v 1.11 84/10/04 12:54:47 rick Exp */
 /* from NetBSD: if_ppp.c,v 1.15.2.2 1994/07/28 05:17:58 cgd Exp */
 
@@ -131,8 +131,6 @@
 #endif
 
 static int	pppsioctl __P((struct ifnet *, int, caddr_t));
-static int	pppoutput __P((struct ifnet *, struct mbuf *,
-			       struct sockaddr *, struct rtentry *));
 static void	ppp_requeue __P((struct ppp_softc *));
 static void	ppp_ccp __P((struct ppp_softc *, struct mbuf *m, int rcvd));
 static void	ppp_ccp_closed __P((struct ppp_softc *));
