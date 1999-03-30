380ddd76d84cf55bfc583858333172eafc1ae425
Paul Mackerras <paulus@samba.org>
1999-03-30 06:02:05 +0000

    alpha -> beta

--- pppd/patchlevel.h
+++ pppd/patchlevel.h
@@ -1,6 +1,6 @@
-/* $Id: patchlevel.h,v 1.36 1999/03/12 06:07:19 paulus Exp $ */
+/* $Id: patchlevel.h,v 1.37 1999/03/30 06:02:05 paulus Exp $ */
 #define	PATCHLEVEL	7
 
 #define VERSION		"2.3"
-#define IMPLEMENTATION	"alpha"
-#define DATE		"26 February 1999"
+#define IMPLEMENTATION	"beta"
+#define DATE		"30 March 1999"
