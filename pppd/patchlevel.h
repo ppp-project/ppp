16d53c1e2eeb81080b1a4bf644fed0e24e27fd99
Paul Mackerras <paulus@samba.org>
2000-08-01 01:46:24 +0000

    update for 2.4.0 release

--- pppd/patchlevel.h
+++ pppd/patchlevel.h
@@ -1,6 +1,6 @@
-/* $Id: patchlevel.h,v 1.49 2000/07/24 14:58:14 paulus Exp $ */
+/* $Id: patchlevel.h,v 1.50 2000/08/01 01:46:24 paulus Exp $ */
 #define	PATCHLEVEL	0
 
 #define VERSION		"2.4"
-#define IMPLEMENTATION	"b5"
-#define DATE		"20 July 2000"
+#define IMPLEMENTATION	""
+#define DATE		"1 August 2000"
