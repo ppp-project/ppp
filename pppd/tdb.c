03af5b9a2cc1264ffbfc4fa8babe95dcc7ef644d
Frank Cusack <fcusack@fcusack.com>
2003-02-24 10:28:00 +0000

    Don't leak tdb file descriptor to scripts.  Bug reported by Steve Grubb.

--- pppd/tdb.c
+++ pppd/tdb.c
@@ -1134,6 +1134,7 @@ TDB_CONTEXT *tdb_open(char *name, int hash_size, int tdb_flags,
             if (tdb.fd == -1) {
 		goto fail;
             }
+	    (void) fcntl(tdb.fd, F_SETFD, FD_CLOEXEC);
         }
 
 	/* ensure there is only one process initialising at once */
