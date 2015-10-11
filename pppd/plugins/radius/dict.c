f323e37f3e6544952f86c5d28afb201b6cbe0ca7
Benjamin Cama <benoar@dolka.fr>
2015-10-11 18:08:11 +0200

    Handle IPv6 RADIUS attributes
    Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>

--- pppd/plugins/radius/dict.c
+++ pppd/plugins/radius/dict.c
@@ -158,6 +158,18 @@ int rc_read_dictionary (char *filename)
 			{
 				type = PW_TYPE_DATE;
 			}
+			else if (strcmp (typestr, "ifid") == 0)
+			{
+				type = PW_TYPE_IFID;
+			}
+			else if (strcmp (typestr, "ipv6addr") == 0)
+			{
+				type = PW_TYPE_IPV6ADDR;
+			}
+			else if (strcmp (typestr, "ipv6prefix") == 0)
+			{
+				type = PW_TYPE_IPV6PREFIX;
+			}
 			else
 			{
 				error("rc_read_dictionary: invalid type on line %d of dictionary %s",
