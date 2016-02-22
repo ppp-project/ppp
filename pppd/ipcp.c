e3d3b310f4cec783210921325ef6237aab92cf65
Stanislav Voroniy <stas@voroniy.com>
2016-02-22 10:35:56 +0100

    Possibility to rename interface in pre-up script from vyatta-ppp

--- pppd/ipcp.c
+++ pppd/ipcp.c
@@ -1788,6 +1788,7 @@ ipcp_up(f)
     ipcp_options *ho = &ipcp_hisoptions[f->unit];
     ipcp_options *go = &ipcp_gotoptions[f->unit];
     ipcp_options *wo = &ipcp_wantoptions[f->unit];
+    int ifindex;
 
     IPCPDEBUG(("ipcp: up"));
 
@@ -1901,8 +1902,18 @@ ipcp_up(f)
 	}
 #endif
 
+	ifindex = if_nametoindex(ifname);
+
 	/* run the pre-up script, if any, and wait for it to finish */
 	ipcp_script(_PATH_IPPREUP, 1);
+
+	/* check if preup script renamed the interface */
+	if (!if_indextoname(ifindex, ifname)) {
+	    if (debug)
+		warn("Interface index %d changed but can't find it");
+	    ipcp_close(f->unit, "Interface configuration failed");
+	    return;
+	}
 
 	/* bring the interface up for IP */
 	if (!sifup(f->unit)) {
