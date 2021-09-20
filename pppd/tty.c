3f4190bb5c21d5feb0474b8f547e93a7f99d270c
Eivind Næss <eivnaes@yahoo.com>
2021-09-20 09:34:15 -0700

    Fixing compiler warnings with regards to GCC, w.r.t. unused results of setuid/setgid
    Signed-off-by: Eivind Næss <eivnaes@yahoo.com>

--- pppd/tty.c
+++ pppd/tty.c
@@ -909,7 +909,7 @@ open_socket(char *dest)
 static int
 start_charshunt(int ifd, int ofd)
 {
-    int cpid;
+    int cpid, ret;
 
     cpid = safe_fork(ifd, ofd, (log_to_fd >= 0? log_to_fd: 2));
     if (cpid == -1) {
@@ -923,10 +923,14 @@ start_charshunt(int ifd, int ofd)
 	    log_to_fd = -1;
 	else if (log_to_fd >= 0)
 	    log_to_fd = 2;
-	setgid(getgid());
-	setuid(uid);
-	if (getuid() != uid)
-	    fatal("setuid failed");
+	ret = setgid(getgid());
+	if (ret != 0) {
+		fatal("setgid failed, %m");
+	}
+	ret = setuid(uid);
+	if (ret != 0 || getuid() != uid) {
+		fatal("setuid failed, %m");
+	}
 	charshunt(0, 1, record_file);
 	exit(0);
     }
